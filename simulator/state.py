e852134d46eeaad439a7c911163c3c61a2cd8c15
ftmshab <ftmshbn76395.gmailcom>
2026-07-14 16:51:18 +0330

    Improved error handling and writing a new function for this purpose during the project.

--- simulator/state.py
+++ simulator/state.py
@@ -17,6 +17,16 @@ class State:
             is_accept: True if this is an accept state.
             is_reject: True if this is a reject state.
         """
+
+        if not name:
+            raise ValueError("State name cannot be empty.")
+
+        if is_accept and is_reject:
+            raise ValueError("State cannot be both accept and reject.")
+
         self.name = name
         self.is_accept = is_accept
         self.is_reject = is_reject
+
+    def __repr__(self) -> str:
+        return self.name
