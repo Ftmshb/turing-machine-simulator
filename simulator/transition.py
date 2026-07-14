8a59735ae19b0463bb0570ddc86c17d32051945b
ftmshab <ftmshbn76395.gmailcom>
2026-07-14 16:54:00 +0330

    Error handling and a print function for use in the project.

--- simulator/transition.py
+++ simulator/transition.py
@@ -1,3 +1,5 @@
+from simulator.constants import LEFT, RIGHT
+
 class Transition:
     """
     Represents a transition in a Turing Machine.
@@ -15,11 +17,20 @@ class Transition:
         Initialize a transition.
         """
 
-        if direction not in ("L", "R"):
-            raise ValueError("Direction must be 'L' or 'R'.")
+        if direction not in (LEFT, RIGHT):
+            raise ValueError("Direction must be LEFT or RIGHT.")
 
         self.current_state = current_state
         self.current_symbol = current_symbol
         self.next_state = next_state
         self.write_symbol = write_symbol
         self.direction = direction
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.current_state} "
+            f"--{self.current_symbol}/"
+            f"{self.write_symbol},"
+            f"{self.direction}--> "
+            f"{self.next_state}"
+        )
